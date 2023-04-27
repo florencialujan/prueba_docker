c26b4b6d76ecdc3360a5de0e32049f470bc6f863
florencialujan <florlujaan@gmail.com>
2023-04-27 16:46:52 -0300

    Primer commit

---
+++ src/main/java/com/example/ejercicio_classe/EjercicioClasseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio_classe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioClasseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioClasseApplication.class, args);
+    }
+
+}
